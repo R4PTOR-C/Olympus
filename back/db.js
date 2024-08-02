fb2637e5e3133ebd25ffa19ef94b814d08ea0f86
R4PTOR-C <rafaelcroriz7@gmail.com>
2024-08-02 11:04:41 -0300

    .

--- back/db.js
+++ back/db.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'olympus_postgresql_user', // usuário do banco de dados remoto
-    host: 'dpg-cptd8sg8fa8c73b7ol70-a.oregon-postgres.render.com', // host do banco de dados remoto
-    database: 'olympus_postgresql', // nome do banco de dados remoto
-    password: 'xhYvhSaZ7ZmOqQCaSShIL80GKIMUxJDe', // senha do banco de dados remoto
+    host: 'dpg-cqmd44rqf0us73aae290-a.oregon-postgres.render.com', // host do banco de dados remoto
+    database: 'olympus_postgresql_q26f', // nome do banco de dados remoto
+    password: '7xaZM4UKyPyZZY5EIvLXtrNBuXJ7xjA9', // senha do banco de dados remoto
     port: 5432, // a porta em que o seu banco de dados remoto está rodando
     ssl: {
         rejectUnauthorized: false // necessário se o servidor requer SSL, mas o certificado é auto-assinado ou não confiável
