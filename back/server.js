52751f9c0e78dae2287def725c33ff2e9c7160c5
Rafael <rafaelcroriz7@gmail.com>
2024-08-26 19:53:35 -0300

    .

--- back/server.js
+++ back/server.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
-const loginRouter = require('./login/login'); // Importa o módulo de login
-const usuariosRouter = require('./usuarios'); // Importa o módulo de usuários
+const loginRouter = require('./login/login');
+const usuariosRouter = require('./usuarios');
 const academiaRouter = require('./academias');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Configurações de Middleware
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000' // Ajuste conforme necessário
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000' // Ajuste conforme necessário
 }));
 
 app.use(express.json());
 
 app.use(session({
-    secret: 'seu_segredo_aqui',
+    secret: process.env.SESSION_SECRET || 'default_secret',
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false } // Em produção, use secure: true e use HTTPS
+    cookie: { secure: process.env.NODE_ENV === 'production' } // Secure cookies in production
 }));
 
 // Middleware para verificar se o usuário está autenticado
@@ -31,15 +31,10 @@ function checkAuthenticated(req, res, next) {
     res.status(401).json({ error: 'Não autorizado' });
 }
 
-// Usar as rotas de login
 app.use('/', loginRouter);
-
-// Usar as rotas de usuários
 app.use('/usuarios', usuariosRouter);
-
 app.use('/academias', academiaRouter);
 
-// Exemplo de uma rota protegida
 app.get('/home', checkAuthenticated, (req, res) => {
     res.json({ message: 'Você está logado e pode ver isso!' });
 });
