47cdd43c18a98466d65e966f65597b8bf4e485fc
R4PTOR-C <rafaelcroriz7@gmail.com>
2024-09-02 09:44:02 -0300

    .

--- back/server.js
+++ back/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Configurações de Middleware
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000' // Ajuste conforme necessário
+    origin: [process.env.FRONTEND_URL || 'http://localhost:3000', 'http://localhost:19000'] // Array de strings para múltiplas origens
 }));
 
 app.use(express.json());
