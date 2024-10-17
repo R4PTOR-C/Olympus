bdc32da21f95289280179e195de1ed29b7d3c9b6
R4PTOR-C <rafaelcroriz7@gmail.com>
2024-10-17 09:25:29 -0300

    alterações para deploy

--- back/server.js
+++ back/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Configurações de Middleware
 app.use(cors({
     credentials: true,
-    origin: [process.env.FRONTEND_URL || 'http://localhost:3000', 'http://localhost:19000'] // Array de strings para múltiplas origens
+    origin: [process.env.FRONTEND_URL || 'http://localhost:3000', 'http://localhost:19000','https://olympus-33lb.onrender.com'] // Array de strings para múltiplas origens
 }));
 
 app.use(express.json());
@@ -46,13 +46,14 @@ app.get('/home', checkAuthenticated, (req, res) => {
     res.json({ message: 'Você está logado e pode ver isso!' });
 });
 
-// Serve arquivos estáticos do build do React
+// Serve arquivos estáticos do React
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Rota catch-all para enviar o index.html para qualquer rota desconhecida
+// Catch-all para redirecionar todas as rotas para o index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 app.listen(PORT, () => {
     console.log(`Servidor backend rodando na porta ${PORT}`);
