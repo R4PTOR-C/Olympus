a610e6921036f762bd2234e815006c7f2c057d0f
Rafael <rafaelcroriz7@gmail.com>
2024-11-07 14:28:14 -0300

    Mudanças de layout

--- front/src/views/components/navbar.js
+++ front/src/views/components/navbar.js
@@ -10,49 +10,41 @@ function Navbar() {
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">OLYMPUS</a>
 
-                {/* Botão de Toggle para exibir a navbar no mobile */}
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
+                <button className="navbar-toggler navbar-toggler-custom" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                {/* Conteúdo colapsável da navbar */}
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     {/* Pesquisa centralizada */}
-                    <div className="search-container">
+                    <div className="mx-auto search-container">
                         <form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
                     </div>
 
-                    {/* Links de navegação e mensagem de usuário */}
-                    <div className="user-links">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            {user.funcao === 'Professor' ? (
+                    {/* Links de navegação alinhados à direita */}
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 user-links">
+                        {user.funcao === 'Professor' ? (
+                            <>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/usuarios">Usuarios</Link>
+                                    <Link className="nav-link" to="/usuarios">Alunos</Link>
                                 </li>
-                            ) : (
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/home">Home</Link>
+                                    <Link className="nav-link" to="#">Exercicios</Link>
                                 </li>
-                            )}
-                        </ul>
+                            </>
+                        ) : (
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/home">Home</Link>
+                            </li>
+                        )}
                         {user.loggedIn && (
-                            <span className="navbar-text me-3">
+                            <span className="navbar-text ms-3">
                                 Olá, {user.userName}!
                             </span>
                         )}
-                        <button className="btn btn-outline-primary" type="button">Logout</button>
-                    </div>
+                    </ul>
                 </div>
             </div>
         </nav>
