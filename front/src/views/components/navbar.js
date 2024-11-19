54d807d765de0db4c8cd34b6afd9d5dffdb70e80
R4PTOR-C <rafaelcroriz7@gmail.com>
2024-11-19 11:29:50 -0300

    correções navbar

--- front/src/views/components/navbar.js
+++ front/src/views/components/navbar.js
@@ -13,64 +13,67 @@ function Navbar() {
         user.logout();
     };
 
+    // Determinar o link para "OLYMPUS" baseado na função do usuário
+    const homeLink = user.funcao === 'Professor' ? '/usuarios' : '/home';
+
     return (
         <nav className="navbar navbar-expand-lg custom-navbar-bg">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">OLYMPUS</a>
+                {/* Link "OLYMPUS" redirecionando condicionalmente */}
+                <Link className="navbar-brand" to={homeLink}>
+                    OLYMPUS
+                </Link>
 
                 <button className="navbar-toggler navbar-toggler-custom" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
-                    {/* Links de navegação alinhados à direita */}
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 user-links">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 user-links align-items-center">
+                        {/* Links de navegação */}
                         {user.funcao === 'Professor' ? (
                             <>
                                 <li className="nav-item">
                                     <Link className="nav-link" to="/usuarios">Alunos</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/exercicios_new">Exercicios</Link>
+                                    <Link className="nav-link" to="/exercicios_new">Exercícios</Link>
                                 </li>
                             </>
                         ) : (
                             <>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/home">Exercicios</Link>
+                                    <Link className="nav-link" to="/home">Exercícios</Link>
                                 </li>
                                 <li className="nav-item">
                                     <Link className="nav-link" to="#">Avaliação Física</Link>
                                 </li>
-                                
                             </>
                         )}
+
+                        {/* Dropdown do usuário */}
                         {user.loggedIn && (
                             <li className="nav-item dropdown ms-3">
-                                {/* Nome do usuário como um botão dropdown */}
                                 <a
-                                    className="nav-link dropdown-toggle"
+                                    className="nav-link dropdown-toggle d-flex align-items-center"
                                     href="#"
                                     id="userDropdown"
                                     role="button"
                                     data-bs-toggle="dropdown"
                                     aria-expanded="false"
                                 >
-                                    Olá, {user.userName}!
+                                    <span>Olá, {user.userName}!</span>
                                     {avatarUrl ? (
                                         <img
                                             src={avatarUrl}
                                             alt="Avatar do usuário"
                                             className="rounded-circle ms-2"
-                                            style={{width: '40px', height: '40px', objectFit: 'cover'}}
+                                            style={{ width: '40px', height: '40px', objectFit: 'cover' }}
                                         />
                                     ) : (
                                         <span className="ms-2"></span>
                                     )}
                                 </a>
-
-                                {/* Dropdown menu */}
                                 <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
                                     <li>
                                         <Link className="dropdown-item" to={`/usuarios/edit/${user.userId}`}>
@@ -78,7 +81,7 @@ function Navbar() {
                                         </Link>
                                     </li>
                                     <li>
-                                        <hr className="dropdown-divider"/>
+                                        <hr className="dropdown-divider" />
                                     </li>
                                     <li>
                                         <button className="dropdown-item" onClick={handleLogout}>
