84f5cda83c2e5707c097eea78e37169fa1df0627
R4PTOR-C <rafaelcroriz7@gmail.com>
2026-03-11 10:22:12 -0300

    env banco de dados

--- back/db.js
+++ back/db.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'olympus_postgresql_user', // usuário do banco de dados remoto
-    host: 'dpg-cqmd44rqf0us73aae290-a.oregon-postgres.render.com', // host do banco de dados remoto
-    database: 'olympus_postgresql_q26f', // nome do banco de dados remoto
-    password: '7xaZM4UKyPyZZY5EIvLXtrNBuXJ7xjA9', // senha do banco de dados remoto
-    port: 5432, // a porta em que o seu banco de dados remoto está rodando
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT) || 5432,
     ssl: {
-        rejectUnauthorized: false // necessário se o servidor requer SSL, mas o certificado é auto-assinado ou não confiável
+        rejectUnauthorized: false
     }
 });
 
